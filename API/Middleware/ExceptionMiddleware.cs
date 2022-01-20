5805a56ad237b674293cf86695dfe58717d02d40
Deepak Jain <deepak.pce1992@gmail.com>
2022-01-20 22:40:16 +0530

    adding an exception handling middleware

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using Application.Core;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IWebHostEnvironment _env;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, IWebHostEnvironment env, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _env = env;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync (HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                var response = _env.IsDevelopment()
+                               ? new AppException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+                               : new AppException(context.Response.StatusCode, "Server Error");
+                
+                // format response to return as JSON
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+                var json = JsonSerializer.Serialize(response, options);
+                await context.Response.WriteAsync(json);
+            }
+        }
+     }
+}
---
+++ Application/Core/AppException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Core
+{
+    public class AppException
+    {
+        public AppException(int statusCode, string error, string detail = null)
+        {
+            StatusCode = statusCode;
+            Error = error; 
+            Detail = detail;
+        }
+        public int StatusCode { get; set; }
+        public string Error { get; set; }
+        public string Detail { get; set; }
+    }
+}
